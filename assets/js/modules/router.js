7b5170a05246b0f888dc9a11451e0bfae572fbb0
Berend Pronk <berendpronk@quicknet.nl>
2017-03-08 00:33:15 +0100

    Refactors router

--- assets/js/modules/router.js
+++ assets/js/modules/router.js
@@ -1,49 +1,17 @@
-var router = {
-	init: function(pagelist) {
-		router.menu(pagelist);
-		router.show(pagelist[0]);
+var router = (function() {
+	var init = function(pagelist) {
+		navigate(pagelist[0]);
 
 		window.addEventListener('hashchange', function() {
 			router.navigate(window.location.hash.replace('#', ''));
 		});
-	},
+	};
 
-	menu: function(pagelist) {
-		var nav = document.querySelector('nav');
-		var ul = document.createElement('ul');
-
-		pagelist.map(function(link) {
-			var li = document.createElement('li');
-			var anchor = document.createElement('a');
-
-			// Early exit to prevent details-page from being rendered in the navigation
-			if (link === 'detail') {
-				return false;
-			}
-
-			anchor.href = '#' + link;
-			if (link === 'send') {
-				anchor.classList.add('current');
-			}
-
-			anchor.textContent = link;
-			anchor.setAttribute('data-anchor', link);
-
-			li.appendChild(anchor);
-			ul.appendChild(li);
-			nav.appendChild(ul);
-		});
-	},
-
-	navigate: function(path) {
+	var navigate = function(path) {
 		var menulinks = utils.convertToArray(document.querySelectorAll('nav a'));
 
 		var hash = '#' + path;
 		window.location.hash = path;
-
-		if (path === 'send') {
-			// window.location.hash = 'send'
-		}
 
 		menulinks.map(function(anchor) {
 			// Retrieves the hash part of the entire anchor-link
@@ -56,10 +24,10 @@ var router = {
 			}
 		});
 
-		this.show(path);
-	},
+		show(path);
+	};
 
-	show: function(path) {
+	var show = function(path) {
 		var sectionList = utils.convertToArray(document.querySelectorAll('body > section'));
 
 		sectionList.map(function(section) {
@@ -69,5 +37,11 @@ var router = {
 				section.classList.add('hidden');
 			}
 		});
-	}
-};
+	};
+
+	return {
+		init: init,
+		navigate: navigate
+	};
+
+}) ();
