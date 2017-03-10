217b7acf914bb77e53b934ac096c29281bf67d43
Berend Pronk <berendpronk@quicknet.nl>
2017-03-10 12:44:24 +0100

    Adds functions to storage module

--- assets/js/modules/storage.js
+++ assets/js/modules/storage.js
@@ -1,8 +1,22 @@
 var storage = (function() {
+	var interestsList = document.querySelector('#interests');
+	var favoritesList = document.querySelector('#favorites');
+	var favControls = document.querySelector('#favorieten .btn-block');
+	var noFavorites = document.querySelector('#noFavorites');
+
+	// Toggles DOM Elements to provice user with feedback
+	var init = function() {
+		if (localStorage.favoritesID) {
+			controls(true);
+		} else {
+			controls(false);
+		}
+	};
+
 	// Checks if result is already in favorites
 	var check = function(id) {
 		if (localStorage.favoritesID) {
-			return utils.checkArray(id, storage.favorites.id()) != -1;
+			return utils.checkArray(id, favorites.id()) != -1;
 		}
 	};
 
@@ -10,8 +24,10 @@ var storage = (function() {
 	var set = function(itemType, item) {
 		// Toggles item in favorites
 		if (item.checked) {
+			controls(true);
+
 			// Checks if favorites list exists
-			if (localStorage.favoritesID) {
+			if (localStorage.favoritesID && (utils.checkArray(item.id, favorites.id()) === -1)) {
 				var currentFavType = localStorage.favoritesType;
 				var currentFavID = localStorage.favoritesID;
 
@@ -28,18 +44,60 @@ var storage = (function() {
 
 	// Removes item from favorites
 	var remove = function(id) {
-		if (checkArray(id, storage.favorites.id()) != -1) {
-			var newTypeStorage = storage.favorites.type();
-			var newIDStorage = storage.favorites.id();
+		if (utils.checkArray(id, favorites.id()) != -1) {
+			var newTypeStorage = favorites.type();
+			var newIDStorage = favorites.id();
 
-			// Removes selected item from localstorage
-			newTypeStorage.splice(utils.checkArray(id, storage.favorites.id()), 1);
-			newIDStorage.splice(utils.checkArray(id, storage.favorites.type()), 1);
+			// Removes selected item from localStorage
+			newTypeStorage.splice(utils.checkArray(id, favorites.id()), 1);
+			newIDStorage.splice(utils.checkArray(id, favorites.type()), 1);
 
 			localStorage.favoritesType = newTypeStorage.join(',');
 			localStorage.favoritesID = newIDStorage.join(',');
 		}
+
+		// Flushes favorite storage if localStorage string is empty
+		if (localStorage.favoritesID == '') {
+			clear();
+			return false;
+		}
+
+		// Clears list before new input comes in
+		utils.clearList(favoritesList);
+
+		// Checks if favorites are empty
+		if (favorites.id().length === 0) {
+			controls(false);
+		}
 	};
+
+	// Removes every favorite
+	var clear = function() {
+		var hearts = document.querySelectorAll('.fav');
+
+		localStorage.clear();
+
+		interestsList.classList.add('hidden');
+		utils.clearList(favoritesList);
+
+		controls(false);
+
+		// Switches checkbox of set favorites to false
+		utils.convertToArray(hearts).map(function(checkbox) {
+			checkbox.checked = false;
+		});
+	};
+
+	// Toggles DOM Elements to provide user with feedback
+	var controls = function(state) {
+		if (state === true) {
+			favControls.classList.remove('hidden');
+			noFavorites.classList.add('hidden');
+		} else {
+			favControls.classList.add('hidden');
+			noFavorites.classList.remove('hidden');
+		}
+	}
 
 	// Returns array of favorites string
 	var favorites = {
@@ -52,11 +110,66 @@ var storage = (function() {
 		}
 	};
 
+	var filters = function(item) {
+		// Object containing values that can be filtered with
+		var opt = {
+			buyPrice: function() {
+				if (item.Koopprijs && !item.Huurprijs) {
+					return item.Koopprijs;
+				} else {
+					return null;
+				}
+			},
+			rentPrice: function() {
+				if (item.Huurprijs && !item.Koopprijs) {
+					return item.Huurprijs;
+				} else {
+					return null;
+				}
+			},
+			area: item.WoonOppervlakte,
+			rooms: item.AantalKamers
+		};
+
+		var filterTypes = ['area', 'rooms', 'buyPrice', 'rentPrice'];
+		var filterValues = [opt.area, opt.rooms, opt.buyPrice(), opt.rentPrice()];
+
+		var newTypes = '';
+		var newValues = '';
+
+		filterTypes.map(function(type, index) {
+			if (newTypes.length !== 0) {
+				newTypes += ',' + type;
+				newValues += ',' + filterValues[index];
+			} else {
+				newTypes = type;
+				newValues = filterValues[index];
+			}
+		});
+
+		if (localStorage.filterTypes) {
+			localStorage.filterTypes += ',' + newTypes;
+			localStorage.filterValues += ',' + newValues;
+
+			// Set local
+			// search.filter.interest(localStorage.filterTypes.split(','), localStorage.filterValues.split(','));
+		} else {
+			localStorage.filterTypes = newTypes;
+			localStorage.filterValues = newValues;
+
+			// Initializes localStorage filters
+			// search.filter.interest(localStorage.filterTypes.split(','), localStorage.filterValues.split(','));
+		}
+	};
+
 	return {
+		init: init,
 		check: check,
 		set: set,
 		remove: remove,
-		favorites: favorites
-	}
+		clear: clear,
+		favorites: favorites,
+		filters: filters
+	};
 
 }) ();
